d0ad61503d74393e724ef9c4c17cf8feeffc0903
Iryna <kuzmina@d-tt.nl>
2024-06-30 16:52:34 +0200

    Initial commit

---
+++ 05-Guess-My-Number/starter/script.js
@@ -0,0 +1,29 @@
+'use strict';
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+  } else if (score > 1) {
+    if (guess === secretNumber) {
+      document.querySelector('.message').textContent = 'Correct number!';
+    } else if (guess < secretNumber) {
+      document.querySelector('.message').textContent = 'Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else if (guess > secretNumber) {
+      document.querySelector('.message').textContent = 'Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    }
+  } else {
+    document.querySelector('.message').textContent = 'You lost the game!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  }
+  console.log(score);
+});
