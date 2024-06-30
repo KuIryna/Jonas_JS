65fcdf7c2c6258dc0694950d760ecaeb9e5bd088
Iryna <kuzmina@d-tt.nl>
2024-06-30 20:01:13 +0200

    Added restart feature

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,16 +1,18 @@
 'use strict';
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
+  //TEMP
+  console.log(secretNumber);
+
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
   if (!guess) {
     document.querySelector('.message').textContent = 'No number!';
   } else if (score > 1) {
     //When player wins
     if (guess === secretNumber) {
+      document.querySelector('.number').textContent = secretNumber;
       document.querySelector('.message').textContent = 'Correct number!';
       document.querySelector('body').style.backgroundColor = '#60b347';
       document.querySelector('.number').style.width = '30rem';
@@ -32,5 +34,18 @@ document.querySelector('.check').addEventListener('click', function () {
     score--;
     document.querySelector('.score').textContent = score;
   }
-  console.log(score);
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+
+  console.log(secretNumber);
 });
